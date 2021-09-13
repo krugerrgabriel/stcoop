e016edd384a76f4edcc4ffa82edba04d6cd5b4d7
Gabriel Sabara Krüger <gabriel.sa.kruger@gmail.com>
2021-09-13 13:59:32 -0300

    13/09/2021 - 13h59

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,25 @@
 module.exports = {
-  presets: ["next/babel"],
-  plugins: [["styled-components", { ssr: true }]],
+  env: {
+    development: {
+      plugins: [
+        [
+          "styled-components",
+          { ssr: true, displayName: true, preprocess: false },
+        ],
+      ],
+      presets: ["next/babel"],
+    },
+    production: {
+      plugins: [
+        [
+          "styled-components",
+          { ssr: true, displayName: true, preprocess: false },
+        ],
+      ],
+      presets: ["next/babel"],
+    },
+  },
+  plugins: [
+    ["styled-components", { ssr: true, displayName: true, preprocess: false }],
+  ],
 };
